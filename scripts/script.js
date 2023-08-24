8aa882baad50ca91dea5c55cd9e9928527b19abe
Chisom <uchisom10@gmail.com>
2023-08-24 06:02:29 -0700

    Added some functionality

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+const gruggy = () => {
+    const name = prompt('what is your name')
+    alert('fuck off ' + name)
+}
+
+const giddy = () => {
+    const name = prompt('what is your name')
+    alert('Welcom ' + name)
+}
